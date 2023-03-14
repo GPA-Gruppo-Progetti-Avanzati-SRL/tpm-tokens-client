4632bb8ff6d5a5416ec294b4c54ff276d12add25
M.A.I <mario.imperato@gpagroup.it>
2023-03-14 18:01:44 +0100

    campaignclient linkedservice

---
+++ campaignclient/linked-service.go
@@ -0,0 +1,17 @@
+package campaignclient
+
+import "github.com/GPA-Gruppo-Progetti-Avanzati-SRL/tpm-http-client/restclient"
+
+type LinkedService struct {
+	cfg *Config
+}
+
+func NewInstanceWithConfig(cfg *Config) (*LinkedService, error) {
+	lks := &LinkedService{cfg: cfg}
+	return lks, nil
+}
+
+func (lks *LinkedService) NewClient(opts ...restclient.Option) (*Client, error) {
+	client, err := NewCampaignApiClient(lks.cfg, opts...)
+	return client, err
+}
